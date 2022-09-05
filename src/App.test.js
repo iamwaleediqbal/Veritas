3774b6c7b3f32d683a44a2154517d1ca2d837f58
iamwaleediqbal <waleedshykh@hotmail.com>
2022-09-05 21:00:25 +0500

    minify html

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import './jquery';
+import './nicepage';
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
