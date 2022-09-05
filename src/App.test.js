8146992b36892166b713097e18dd4d6f45239dcf
iamwaleediqbal <waleedshykh@hotmail.com>
2022-09-05 22:35:37 +0500

    Fix javascript paths

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import './jquery';
-import './nicepage';
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
